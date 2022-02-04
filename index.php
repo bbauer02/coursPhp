4900b89f7ebb757bfc423f65869b5c23cb5922c7
guillaume <grocher@orbleucom.fr>
2022-02-04 09:34:43 +0100

    ajout methode modifier suppression dans la class PersonnageManager

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require_once('./include/class/PersonnageManager.php');
 use \Studio\Lotr\v1\Magicien as MagicienV1;
 use \Studio\Lotr\v1\Guerrier;
 use \Studio\Lotr\v2\Magicien as MagicienV2;
+use \Studio\Lotr\v2\Guerrier as GuerrierV2;
 ?>
 
 <h1>Street Fighter PHP ^^</h1>
@@ -26,9 +27,23 @@ $Gandalf = new MagicienV1("Gandalf", 200, 150);
 $Aragorn = new Guerrier("Aragorn", 200, 150);
 $Gandalfv2 = new MagicienV2("Gandalf le Blanc", 200, 150);
 
+$Aragornv2 = new GuerrierV2("Aragorn armé", 200, 200);
+
+$Aragornv2->swordAttack($Gandalfv2);
+var_dump($Aragornv2);
+
+
 $manager = new PersonnageManager();
 var_dump($manager);
-$manager->save($Aragorn );
+
+//$manager->save($Aragorn);
+
+
+$Aragorn = new Guerrier("Aragorn", 100, 250);
+
+$manager->edit($Aragorn);
+
+//$manager->delete($Aragorn);
 
 
 
